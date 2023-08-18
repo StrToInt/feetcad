c3538d8e3c786cd3d2c10e4406bc9bd510f254bb
Cj <jekich.es@gmail.com>
2023-08-18 21:07:16 +1000

    много всего нужного

--- feetcad.py
+++ feetcad.py
@@ -9,6 +9,7 @@ from pyglet.graphics import Group
 from pyglet.math import Vec2
 import time
 import pyglet.gl as gl
+from tkinter import Tk, Frame, Menu
 
 class CameraGroup(Group):
     """ Graphics group emulating the behaviour of a camera in 2D space. """
@@ -140,10 +141,10 @@ class SCHEME:
 
 class FEETCAD(pyglet.window.Window):
 
-    def __init__(self):
+    def __init__(self, macro_edit = False, macro_edit_window = None):
         config = pyglet.gl.Config()
         config.sample_buffers = 1
-        config.samples = 4
+        config.samples = 1
         super(FEETCAD,self).__init__(720, 480, "FEETCAD",config=config,resizable=True,style=pyglet.window.Window.WINDOW_STYLE_DEFAULT)
         self.time = 0
         self.__grid_batch = pyglet.graphics.Batch()
@@ -151,15 +152,18 @@ class FEETCAD(pyglet.window.Window):
         self.scheme = SCHEME()
         self.batch = pyglet.graphics.Batch()
         self.camera = CenteredCameraGroup(self,0,0,1)
+        self.camera_hud = CameraGroup(self,0,0,1)
         self.__clickTime = time.time()
 
         self.__grid_shapes_big = []
         self.__grid_shapes_small = []
-        self.__grid_camera = CameraGroup(self,0,0,1)
 
         self.__shapes_hud = []
         self.__grid_visible = False
         self.__last_grid_state = True
+
+        self.magnifier = 10.0
+
         self.grid_width = 2
         self.grid_step = 1
         self.grid_steps = 60
@@ -173,10 +177,52 @@ class FEETCAD(pyglet.window.Window):
         self.under_mouse_shapes = []
 
         self.zoom_step = 5
+        self.cursor = shapes.Circle(20, 20, 2, color=(255,255,255,50), batch = self.batch, group = self.camera)
 
 
         self.generate_grid()
 
+        self.hilighted_components = []
+
+        self.miny,self.minx,self.maxy,self.maxx = (100,100,-100,-100)
+
+        self.toggle_grid()
+
+        self.clearScheme()
+
+        self.macro_edit = macro_edit
+
+        if macro_edit:
+            self.set_visible(False)
+
+        self.macro_edit_window = macro_edit_window
+
+
+    def check_for_macro_edit(self, enter_to_edit = True):
+        if enter_to_edit:
+            if len(self.hilighted_components) == 1:
+                self.in_macro_edit = self.hilighted_components[0]
+                in_macro = FEETCAD()
+
+                if 'components' in self.scheme.jsonData:
+                    for component in self.scheme.jsonData['components']:
+
+                        for shape in component['temp_shapes'].shapes:
+                            color = shape.color
+                            if len(color) == 4:
+                                shape.color = (color[0],color[1],color[2],50)
+
+                self.loadShapesFromJson(self.hilighted_components[0])
+                print("self.in_macro_edit['x'],self.in_macro_edit[y]",self.in_macro_edit['x'],self.in_macro_edit['y'])
+        else:
+            print('exiting macro edit')
+            self.in_macro_edit = None
+            self.loadShapesFromJson()
+
+        for shape in self.in_macro_edit_shapes:
+            shape.visible = self.in_macro_edit != None
+
+        self.recalculate_grid()
 
     def initialize_hud(self):
         __shapes_hud = []
@@ -209,7 +255,7 @@ class FEETCAD(pyglet.window.Window):
         newx = self.camera.x
         newy = self.camera.y
 
-        cell_width = self.grid_step*self.camera.zoom
+        cell_width = self.grid_step*self.camera.zoom/self.magnifier
         #print('cell width',cell_width)
         #normal: between 10 and 100
         zoom_factor = 1
@@ -224,9 +270,10 @@ class FEETCAD(pyglet.window.Window):
         #print('new cell width',cell_width)
 
         middlex = (self.grid_steps*self.grid_step*zoom_factor)/2
+
         middley = (self.grid_steps*self.grid_step*zoom_factor)/2
 
-
+        #vertical lines
         x = 0
         for line in self.__grid_shapes_big[1]:
             line.x = x*self.grid_step*zoom_factor-middlex+self.camera.x-self.camera.x%(zoom_factor*self.grid_step)
@@ -242,16 +289,19 @@ class FEETCAD(pyglet.window.Window):
 
             line.width = self.grid_width/self.camera.zoom
             line.x-=line.width/2
+
+
             x+=1
 
             if not self.__grid_visible:
                 line.x+=1000000
 
+        #horizontal lines
         y = 0
         for line in self.__grid_shapes_big[0]:
             line.y = y*self.grid_step*zoom_factor-middley+self.camera.y-self.camera.y%(zoom_factor*self.grid_step)
 
-            if line.x  == 0:
+            if line.y  == 0:
                 line.color = self.grid_zero_color
             elif line.y % (self.grid_zoom_step*zoom_factor) == 0:
                 line.color = self.grid_primary_color
@@ -262,6 +312,7 @@ class FEETCAD(pyglet.window.Window):
 
             line.height = self.grid_width/self.camera.zoom
             line.y-=line.height/2
+
             y+=1
 
             if not self.__grid_visible:
@@ -287,8 +338,14 @@ class FEETCAD(pyglet.window.Window):
     def reset_view(self):
         #calc zoom
         #WITH BORDER 20PX
-        scheme_width = self.maxx - self.minx+20
-        scheme_height = self.maxy - self.miny+20
+
+        maxx = self.maxx
+        minx = self.minx
+        maxy = self.maxy
+        miny = self.miny
+
+        scheme_width = maxx - minx+20
+        scheme_height = maxy - miny+20
         max_dim_scheme, max_dim_window = (0, 0)
 
         if scheme_width > scheme_height:
@@ -300,12 +357,31 @@ class FEETCAD(pyglet.window.Window):
 
         zoom = max_dim_window/max_dim_scheme
         self.camera.zoom=zoom
-        self.camera.x = self.minx+(self.maxx-self.minx)/2
-        self.camera.y = self.miny+(self.maxy-self.miny)/2
+        self.camera.x = minx+(maxx-minx)/2
+        self.camera.y = miny+(maxy-miny)/2
         self.recalculate_grid()
 
-    def on_mouse_motion(x, y, dx, dy):
-        pass
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.cursor.x = (-self.width/2+x)/self.camera.zoom+self.camera.x
+        self.cursor.y = (-self.height/2+y)/self.camera.zoom+self.camera.y
+        #print("mouse on canvas: x,y:",self.cursor.x,self.cursor.y)
+        #print('len(self.hilighted_components)',len(self.hilighted_components));
+        selected = self.check_mouse_onshape(self.cursor.x, self.cursor.y)
+        #print(selected)
+
+        for component in self.hilighted_components:
+            #print('last',component['name'])
+            self.loadShapesFromJson(component)
+
+        if len(selected) > 0:
+            for component in selected:
+                #print('new',selected)
+                for shape in component['temp_shapes'].shapes:
+                    shape.color = [255,0,0,255]
+
+        self.hilighted_components = selected
+
+
 
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         dx = (self.width/2-x)
@@ -356,16 +432,18 @@ class FEETCAD(pyglet.window.Window):
             else:
                 self.__clickTime = time.time()
 
+        if button == pyglet.window.mouse.LEFT:
+            t = time.time()
+            if t - self.__clickTime < 0.25:
+                self.check_for_macro_edit()
+            else:
+                self.__clickTime = time.time()
 
     def on_draw(self):
         """Clear the screen and draw shapes"""
+        #check macroedit mode
         self.clear()
         self.batch.draw()
-
-    def update(self, delta_time):
-        """Animate the shapes"""
-        self.time += delta_time
-        self.circle.radius = 75 + math.sin(self.time * 1.17) * 25
 
     def on_key_press(self, symbols, modifiers):
         if pyglet.window.key.MOD_SHIFT & modifiers and \
@@ -377,14 +455,64 @@ class FEETCAD(pyglet.window.Window):
             print('ctrl')
             self.toggle_grid()
 
+        if symbols == pyglet.window.key.ESCAPE:
+            if self.in_macro_edit != None:
+                self.check_for_macro_edit(False)
+
     def clearScheme(self):
         self.shapes = []
 
+    def check_mouse_onshape(self, mousex, mousey):
 
-    def loadShapesFromJson(self):
+        def mouseInRect(x1,y1,x2,y2, mousex,mousey, tolerance):
+            tolerance*=self.magnifier
+
+            if x1>x2:
+                b = x1
+                x1 = x2
+                x2 = b
+            if y1>y2:
+                b = y1
+                y1 = y2
+                y2 = b
+
+            x1-=tolerance
+            x2+=tolerance
+            y1-=tolerance
+            y2+=tolerance
+
+            if  mousex >= x1 and mousex <= x2 and\
+                mousey >= y1 and mousey <= y2:
+                    return True
+            return False
+
+        selected = []
+
+        if self.scheme.jsonData and 'components' in self.scheme.jsonData:
+            for component in self.scheme.jsonData['components']:
+                if isinstance(component['temp_shapes'],SCHEME_DRAW_ITEM):
+                    minx = 0
+                    miny = 0
+                    maxx = 0
+                    maxy = 0
+                    temp_shapes = component['temp_shapes']
+                    for shape in temp_shapes.shapes:
+                        if isinstance(shape, shapes.Line):
+                            if mouseInRect(shape.x,shape.y,shape.x2,shape.y2,mousex,mousey,0.5):
+                                #print('component',component['name'],'has shape under mouse: x,y',shape.x,shape.y,str(shape))
+                                selected.append(component)
+                                break
+                        if isinstance(shape, shapes.Rectangle):
+                            if mouseInRect(shape.x,shape.y,shape.x+shape.width,shape.y+shape.height,mousex,mousey,0.5):
+                                #print('component',component['name'],'has shape under mouse: x,y',shape.x,shape.y,str(shape))
+                                selected.append(component)
+                                break
+        return selected
+
+
+    def loadShapesFromJson(self, targetComponent = None, onlyBounds = False):
         if self.scheme.jsonData != None:
             self.__schemeBatch = pyglet.graphics.Batch()
-            self.miny,self.minx,self.maxy,self.maxx = (100,100,-100,-100)
 
             def copare_bounds(x1,x2,y1,y2):
                 #nonlocal maxx,minx,maxy,miny
@@ -399,82 +527,133 @@ class FEETCAD(pyglet.window.Window):
                 if y1 > self.maxy: self.maxy = y1
                 if y2 > self.maxy: self.maxy = y2
 
-            if 'components' in self.scheme.jsonData:
-                for component in self.scheme.jsonData['components']:
-                    x = component['x']
-                    y = component['y']
+            def loadShapesFromComponent(component, onlyBounds = False):
+                x0 = component['x']
+                y0 = component['y']
+
+                boundminx, boundminy, boundmaxx, boundmaxy = (100000,100000,-100000,-100000)
+
+                def compare_internal_bounds(x1,x2,y1,y2):
+                    nonlocal boundmaxx, boundmaxy, boundminx, boundminy
+
+                    if x1 < boundminx: boundminx = x1
+                    if x2 < boundminx: boundminx = x2
+                    if x1 > boundmaxx: boundmaxx = x1
+                    if x2 > boundmaxx: boundmaxx = x2
+
+                    if y1 < boundminy: boundminy = y1
+                    if y2 < boundminy: boundminy = y2
+                    if y1 > boundmaxy: boundmaxy = y1
+                    if y2 > boundmaxy: boundmaxy = y2
+
+                if onlyBounds == False:
                     component['temp_shapes'] = SCHEME_DRAW_ITEM()
-                    if 'shapes' in component:
-                        for shape in component['shapes']:
-                            if shape['type'] == "line":
-                                x1 = shape['x1']+x
-                                x2 = shape['x2']+x
-                                y1 = shape['y1']+y
-                                y2 = shape['y2']+y
-                                copare_bounds(x1,x2,y1,y2)
 
-                                line = shapes.Line(         x1, y1,\
-                                                            x2,y2,\
-                                                            width=shape['width'],\
-                                                            color=(shape['color'][0], shape['color'][1], shape['color'][2],shape['color'][3]),\
-                                                            batch=self.batch,\
-                                                            group=self.camera)
-                                #self.__shapes.append(line)
+                if 'shapes' in component:
+                    for shape in component['shapes']:
+                        if shape['type'] == "line":
+                            x1 = shape['x1']+x0
+                            x2 = shape['x2']+x0
+                            y1 = shape['y1']+y0
+                            y2 = shape['y2']+y0
+
+                            x1*=self.magnifier
+                            x2*=self.magnifier
+                            y1*=self.magnifier
+                            y2*=self.magnifier
+
+                            copare_bounds(x1,x2,y1,y2)
+                            compare_internal_bounds(x1,x2,y1,y2)
+
+                            line = shapes.Line(         x1, y1,\
+                                                        x2,y2,\
+                                                        width=shape['width']*self.magnifier,\
+                                                        color=(shape['color'][0], shape['color'][1], shape['color'][2],shape['color'][3]),\
+                                                        batch=self.batch,\
+                                                        group=self.camera)
+                            #self.__shapes.append(line)
+                            if onlyBounds == False:
                                 component['temp_shapes'].addItem(line)
 
-                            if shape['type'] == "rectangle":
-                                x1 = shape['x1']+x
-                                x2 = shape['x2']+x
-                                y1 = shape['y1']+y
-                                y2 = shape['y2']+y
-                                copare_bounds(x1,x2,y1,y2)
+                        if shape['type'] == "rectangle":
+                            x1 = shape['x1']+x0
+                            x2 = shape['x2']+x0
+                            y1 = shape['y1']+y0
+                            y2 = shape['y2']+y0
 
-                                color=(shape['color'][0], shape['color'][1], shape['color'][2],shape['color'][3])
+                            x1*=self.magnifier
+                            x2*=self.magnifier
+                            y1*=self.magnifier
+                            y2*=self.magnifier
 
-                                line = shapes.Line( x1,y1,x2,y1,\
-                                                    width=shape['width'],\
-                                                    color=color,\
-                                                    batch=self.batch,\
-                                                    group=self.camera)
-                                #self.__shapes.append(line)
-                                component['temp_shapes'].addItem(line)
+                            copare_bounds(x1,x2,y1,y2)
+                            compare_internal_bounds(x1,x2,y1,y2)
 
-                                line = shapes.Line( x2,y1,x2,y2,\
-                                                    width=shape['width'],\
-                                                    color=color,\
-                                                    batch=self.batch,\
-                                                    group=self.camera)
-                                #self.__shapes.append(line)
-                                component['temp_shapes'].addItem(line)
+                            color=(shape['color'][0], shape['color'][1], shape['color'][2],shape['color'][3])
 
-                                line = shapes.Line( x1,y1,x1,y2,\
-                                                    width=shape['width'],\
-                                                    color=color,\
-                                                    batch=self.batch,\
-                                                    group=self.camera)
-                                #self.__shapes.append(line)
-                                component['temp_shapes'].addItem(line)
+                            rect = shapes.Rectangle(x1,y1,x2-x1,y2-y1,color,\
+                                                batch=self.batch,\
+                                                group=self.camera)
 
-                                line = shapes.Line( x1,y2,x2,y2,\
-                                                    width=shape['width'],\
-                                                    color=color,\
-                                                    batch=self.batch,\
-                                                    group=self.camera)
-                                #self.__shapes.append(line)
-                                component['temp_shapes'].addItem(line)
+                            if onlyBounds == False:
+                                component['temp_shapes'].addItem(rect)
 
-                print( 'minx,maxx,miny,maxy:', self.minx, self.maxx, self.miny, self.maxy)
+                if 'labels' in component:
+                    for label in component['labels']:
+                        x = label['x']+x0
+                        y = label['y']+y0
+
+
+                        x*=self.magnifier
+                        y*=self.magnifier
+
+                        copare_bounds(x1,x2,y1,y2)
+                        compare_internal_bounds(x1,x2,y1,y2)
+
+                        text = ''
+
+                        if label['field'] == 'name':
+                            text = component["name"]
+                        else:
+                            text = label['text']
+
+                        if 'field_visible' in label and label['field_visible'] == True:
+                            text = label['field'] + ":" + text
+
+
+                        label = segoe_ui_semilbold = pyglet.text.Label(text,\
+                            font_name=label['font']['name'],\
+                            bold="semibold",\
+                            font_size=label['font']['size']*self.magnifier,\
+                            x=x,\
+                            y=y,\
+                            batch=self.batch,
+                            group = self.camera)
+                        #self.__shapes.append(line)
+
+                        if onlyBounds == False:
+                            component['temp_shapes'].addItem(label)
+                if onlyBounds == True:
+                    return (boundminx,boundminy,boundmaxx,boundmaxy)
+
+            if targetComponent == None:
+                if 'components' in self.scheme.jsonData:
+                    for component in self.scheme.jsonData['components']:
+                        loadShapesFromComponent(component)
+            else:
+                if onlyBounds:
+                    return loadShapesFromComponent(targetComponent, True)
+                else:
+                    loadShapesFromComponent(targetComponent)
 
 
 
 if __name__ == "__main__":
-    cad = FEETCAD()
-    cad.scheme = SCHEME()
-    cad.clearScheme()
-    cad.scheme.loadScheme('test.jschem')
+    macro_window = FEETCAD(macro_edit = True)
+    cad = FEETCAD(macro_edit_window = macro_window)
+    cad.scheme.loadScheme('test.json')
     cad.loadShapesFromJson()
     cad.reset_view()
-    #cad.initialize_hud()
-    cad.toggle_grid()
-    cad.scheme.saveScheme('test.jschem')
+    cad.initialize_hud()
+    cad.scheme.saveScheme('test_out.jschem')
     pyglet.app.run()
